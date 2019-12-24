a959db91f6fdd14fc67f2863d7b5e6ec8504cc21
Cam Prier <CSPrier@gmail.com>
2019-12-24 11:55:59 -0500

    Details about the Stack added

--- Stack.js
+++ Stack.js
@@ -1,11 +1,29 @@
 'use strict';
 
+/**
+ * Node class.
+ * Holds the data of a node, and the data of the next node in the stack, too.
+ */
+
 class _Node {
   constructor(data, next) {
     this.data = data;
     this.next = next;
   }
 }
+
+/**
+ * Stack class.
+ * Has a top value.
+ * When you first instantiate the class, it has a null value for the top, because there is nothing on the Stack.
+ * When you push() to the Stack, and it's the first Node added to the Stack, 
+ *  the top is initialized the value of a new Node, with the data passed into the push().
+ *  Otherwise, create a new Node and initialize the Stack's top to be the new Node.
+ * To pop() a node off the top of the Stack,
+ *  initialize a variable called 'node' with the value of the node on top of the Stack, 
+ *  initialize the top of the Stack to be the value of the next node in the Stack,
+ *  then return the top node's data.
+ */
 
 class Stack {
   constructor() {
@@ -30,3 +48,16 @@ class Stack {
 }
 
 module.exports = Stack;
+
+/**
+ * 
+ Stack{
+  top: Node{ 
+        data: 1, 
+        next: Node{ 
+                data: 2, 
+                next: null 
+              } 
+            }
+      };
+ */
